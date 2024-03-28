e55e85f4af0ef1a387fd1373c41ced40bfebc7e6
bunny6677 <165284399+bunny6677@users.noreply.github.com>
2024-03-28 18:45:08 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -12,7 +12,6 @@ class Var(object):
     API_ID = int(getenv('API_ID'))
     API_HASH = str(getenv('API_HASH'))
     BOT_TOKEN = str(getenv('BOT_TOKEN'))
-    API = str(getenv('API'))
     SESSION_NAME = str(getenv('SESSION_NAME', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
